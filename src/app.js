86aebcccc70337c5611bef0ec2835f7c2a7b068e
Briggitte Merchan <briggittemerchan6@gmail.com>
2022-10-12 11:15:40 -0500

    init process

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
+//? Dependencies
 const express = require('express')
 
+
+//? files
+const {port} = require('./config')
+
+
+//? Initial configs
 const app = express()
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Ok!', users: `localhost:${port}/api/v1/users`})
+})
+
+
+
+
+//? inital server 
+app.listen(3000, () => {
+    console.log(`Server started port ${port}`)
+})
---
+++ src/users/users.controler.js
@@ -0,0 +1,32 @@
+const Users = require('../models/users.models')
+const { hashPassword } = require('../utils/crypto')
+
+const getAllUsers = async () => {
+    const data = await Users.findAll()
+    return data
+}
+
+
+const getUserById = async (id) => {
+    const data = await Users.findOne({
+        where: {
+            id: id
+        }
+    })
+    return data
+}
+
+
+const createUser = async (data) => {
+    const newUser = await Users.create({
+        id: uuid.v4,
+        password: hashPassword(data.password)
+    })
+    return newUser
+}
+
+module.exports = {
+    getAllUsers,
+    getUserById, 
+    createUser
+}
