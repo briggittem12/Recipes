4535a4c5ef8e5e019af71c7bb6c15bc96945835f
Briggitte Merchan <briggittemerchan6@gmail.com>
2022-11-02 22:04:49 -0500

    recipes and other resources

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //? Dependencies
 const express = require('express')
 const db = require('./utils/database')
+const cors = require('cors')
 
 //? files
 const {port} = require('./config')
@@ -9,12 +10,15 @@ const authRouter = require('./auth/auth.routes')
 const categorysRouter = require('./categories/categories.router')
 const typesRouter = require('./types/types.router')
 const ingredientsRouter = require('./ingredients/ingredients.router')
+const recipesRouter = require('./recipes/recipes.router')
 const initModels = require('./models/initModels')
 
 //? Initial configs
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 db.authenticate()
     .then(() => {console.log('Database Authernticate')})
@@ -37,9 +41,10 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/users', usersRouter)
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/categorys', categorysRouter)
+app.use('/api/v1/categories', categorysRouter)
 app.use('/api/v1/types', typesRouter)
 app.use('/api/v1/ingredients', ingredientsRouter)
+app.use('/api/v1/recipes', recipesRouter)
 
 
 
--- src/ingredients/ingredients.router.js
+++ src/ingredients/ingredients.router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
+
 const ingredientService = require('./ingredients.services')
+
 
 router.route('/')
         .get(ingredientService.getAllIngredients)
@@ -10,6 +12,11 @@ router.route('/:id')
         .get(ingredientService.getIngredientsById)
         .patch(ingredientService.updateIngredients)
         .delete(ingredientService.deleteIngredients)
+
+router.post('/:ingredient_id/add_to_user', 
+        passport.authenticate('jwt', {session: false})
+)
+
 
 
 module.exports = router
